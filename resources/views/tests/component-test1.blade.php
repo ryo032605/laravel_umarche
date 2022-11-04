010aa753b30fd199f5dd2c792862ea5fcdd7abb5
ryotanaka <larc.enable-53@outlook.com>
2022-11-04 15:00:17 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,4 +2,6 @@
 <x-slot name="header">ヘッター１</x-slot>
 コンポーネントテスト１
 <x-tests.card title="タイトル1" content="本文1" :message="$message"/>
+<x-tests.card title="タイトル2"/>
+<x-tests.card title="CSSを変更したい" class="bg-red-300"/>
 </x-tests.app>
