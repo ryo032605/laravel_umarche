3bb2b4920abedd0f8afaf787584ec34e1db747d7
ryotanaka <larc.enable-53@outlook.com>
2022-11-15 15:06:57 +0900

    product store

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-
 class ProductSeeder extends Seeder
 {
     /**
@@ -20,27 +19,27 @@ class ProductSeeder extends Seeder
                 'shop_id' => 1,
                 'secondary_category_id' => 1,
                 'image1' => 1,
-            ], 
+            ],
             [
                 'shop_id' => 1,
                 'secondary_category_id' => 2,
                 'image1' => 2,
-            ], 
+            ],
             [
                 'shop_id' => 1,
                 'secondary_category_id' => 3,
                 'image1' => 3,
-            ], 
+            ],
             [
                 'shop_id' => 1,
                 'secondary_category_id' => 4,
                 'image1' => 3,
-            ], 
+            ],
             [
                 'shop_id' => 1,
                 'secondary_category_id' => 5,
                 'image1' => 4,
-            ], 
+            ],
         ]);
     }
 }
