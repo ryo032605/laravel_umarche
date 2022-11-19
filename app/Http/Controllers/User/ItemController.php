75d8857dcb081f32442a4a490d2ed270aae046ab
ryotanaka <larc.enable-53@outlook.com>
2022-11-19 15:29:26 +0900

    表示順

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -24,8 +24,9 @@ class ItemController extends Controller
                  return $next($request);
         });
     }
-   public function index(){
-    $products = Product::availableItems()->get();
+   public function index(Request $request){
+    $products = Product::availableItems()->sortOrder($request->sort)
+    ->get();
     
     return view('user.index',compact('products'));
    }
