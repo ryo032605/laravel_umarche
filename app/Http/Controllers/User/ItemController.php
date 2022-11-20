54155ae6c45e2d0acba83fcc04680f647734f772
ryotanaka <larc.enable-53@outlook.com>
2022-11-20 12:23:13 +0900

    テストメール

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 use App\Models\PrimaryCategory;
+use App\Mail\TestMail;
 
 class ItemController extends Controller
 {
@@ -26,7 +28,10 @@ class ItemController extends Controller
         });
     }
    public function index(Request $request){
+
     //dd($request);
+    Mail::to('test@example.com')
+    ->send(new testMail());
     $categories = PrimaryCategory::with('secondary')
     ->get();
     $products = Product::availableItems()
